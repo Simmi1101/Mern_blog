e13d8fc77d50e656007f8a641c8361a6977c5d6b
Simmi <simmi.chaudhary11@gmail.com>
2024-09-01 16:05:39 +0530

    first commit

---
+++ client/src/pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        About
+      </h1>
+      <p className="mx-auto leading-relaxes text-base mb-4">
+        Pariatur nisi ipsum cillum dolor sint cupidatat. Anim veniam laboris
+        enim cillum culpa id anim enim id. Labore ex dolor eiusmod ad minim non
+        eiusmod fugiat commodo dolore fugiat proident elit qui. Sint
+        reprehenderit exercitation aute tempor exercitation officia ipsum cillum
+        duis enim amet exercitation magna incididunt. Laboris sunt velit
+        pariatur culpa mollit do duis laboris Lorem nulla adipisicing qui
+        dolore.
+      </p>
+      <p className="mx-auto leading-relaxes text-base mb-4">
+        Occaecat consequat ad incididunt irure non sint do officia cupidatat
+        anim aute nostrud aliqua enim. Dolore tempor ut duis culpa pariatur
+        culpa mollit officia eu. Cupidatat est cupidatat officia exercitation.
+        Labore Lorem mollit dolor ea veniam consectetur ea proident commodo sunt
+        mollit veniam occaecat.
+      </p>
+      <p className="mx-auto leading-relaxes text-base mb-4">
+        Nulla ipsum eiusmod dolore dolor nulla nostrud. Nisi id eu minim velit
+        do pariatur in consequat culpa labore ad ea. Sunt et nisi excepteur
+        dolor. Duis exercitation ex occaecat eu in non fugiat consequat commodo
+        culpa. Enim laboris ad amet eu excepteur veniam culpa. Ut eu veniam id
+        labore aliquip labore pariatur sit minim minim ad ad.
+      </p>
+    </div>
+  );
+};
+
+export default About;
